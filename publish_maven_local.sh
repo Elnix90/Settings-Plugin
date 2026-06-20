95ef06ab3c61898f8c0017e531b7ae8fd195448a
Elnix90 <elnix91@proton.me>
2026-06-20 10:07:29 +0200

    fix: updated scripts and creds

--- publish_maven_local.sh
+++ publish_maven_local.sh
@@ -1,4 +1,6 @@
-export ORG_GRADLE_PROJECT_signingInMemoryKeyPassword="$(cat passwd.txt)"
-export ORG_GRADLE_PROJECT_signingInMemoryKey="$(cat private.asc)"
+export ORG_GRADLE_PROJECT_signingInMemoryKeyPassword="$(cat creds/passwd.txt)"
+export ORG_GRADLE_PROJECT_signingInMemoryKey="$(cat creds/private.asc)"
+export ORG_GRADLE_PROJECT_mavenCentralUsername="$(cat creds/maven_passwd.txt)"
+export ORG_GRADLE_PROJECT_mavenCentralPassword="$(cat creds/maven_username.txt)"
 
 ./gradlew publishToMavenLocal --stacktrace
